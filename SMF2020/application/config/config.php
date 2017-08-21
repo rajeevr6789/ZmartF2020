7be8775d1f7cc4e78ee6793a4d8c1d750b953089
rajeevr6789 <30588143+rajeevr6789@users.noreply.github.com>
2017-08-21 17:21:03 +0530

    Updated base url

--- SMF2020/application/config/config.php
+++ SMF2020/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/SMF2020/';
+$config['base_url'] = 'http://localhost/ZmartF2020/trunk/SMF2020/';
 
 /*
 |--------------------------------------------------------------------------
